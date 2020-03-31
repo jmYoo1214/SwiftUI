0b51972fea9a5e8e7be648ef044db74454b4aee0
유종민이 <dbwhdals1214@gmail.com>
2020-03-31 14:06:24 +0900

    folder rename

--- swiftUI/SwiftUIView.swift
+++ swiftUI/SwiftUIView.swift
@@ -8,12 +8,17 @@
 
 import SwiftUI
 
+// view 프로토콜을 준수하고 있으며 뷰의 컨텐츠 와 레이아웃을 설명해주고 있는 부분
 struct SwiftUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       Text("Turtle Rock!")
+        .font(.title).foregroundColor(Color.red)
+        
     }
 }
 
+
+// 해당 뷰에 대한 미리보기를 선언.
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
         SwiftUIView()
