05ac537b9b39604c86dbdac6527b6b8e4ceac645
유종민이 <dbwhdals1214@gmail.com>
2020-03-31 14:01:40 +0900

    frist

---
+++ hairColorChange/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  hairColorChange
+//
+//  Created by 유종민이 on 2020/03/31.
+//  Copyright © 2020 유종민이. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
