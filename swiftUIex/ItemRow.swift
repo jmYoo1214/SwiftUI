10e9ce2670afc2905980c14f3ba0bf6a542d9965
유종민이 <dbwhdals1214@gmail.com>
2020-04-02 16:15:47 +0900

    NavigationEx

---
+++ swiftUIex/ItemRow.swift
@@ -0,0 +1,21 @@
+//
+//  ItemRow.swift
+//  swiftUIex
+//
+//  Created by 유종민이 on 2020/04/02.
+//  Copyright © 2020 유종민이. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow()
+    }
+}
