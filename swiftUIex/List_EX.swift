39b32e19a662700411931ae6e27cf9a2c2059166
유종민이 <dbwhdals1214@gmail.com>
2020-04-02 10:58:36 +0900

    list swiftUI

---
+++ swiftUIex/List_EX.swift
@@ -0,0 +1,65 @@
+//
+//  combineEX.swift
+//  swiftUIex
+//
+//  Created by 유종민이 on 2020/04/01.
+//  Copyright © 2020 유종민이. All rights reserved.
+// https://www.hackingwithswift.com/quick-start/swiftui/building-a-menu-using-list
+
+import SwiftUI
+
+struct combineEX: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
+    var body: some View {
+        NavigationView{
+            List{
+                ForEach(menu) { section in
+                    Section(header: Text(section.name)) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }.navigationBarTitle(Text("Menu"))
+            .listStyle(GroupedListStyle())
+        }
+    }
+}
+
+struct combineEX_Previews: PreviewProvider {
+    static var previews: some View {
+        combineEX()
+    }
+}
+
+
+extension Bundle {
+    func decode<T: Decodable>(_ type: T.Type, from file: String, dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate, keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = dateDecodingStrategy
+        decoder.keyDecodingStrategy = keyDecodingStrategy
+
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' not found – \(context.debugDescription)")
+        } catch DecodingError.typeMismatch(_, let context) {
+            fatalError("Failed to decode \(file) from bundle due to type mismatch – \(context.debugDescription)")
+        } catch DecodingError.valueNotFound(let type, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing \(type) value – \(context.debugDescription)")
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON")
+        } catch {
+            fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
+        }
+    }
+}
